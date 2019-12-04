4057894932785e9dd251cede684a82de06e9cd34
Juri Perwuschin <juriperwuschin@gmail.com>
2019-12-04 10:33:05 +0100

    added initial files
    all files  created before the repo

---
+++ ch1/pieChart.js
@@ -0,0 +1,25 @@
+var data = [
+    { data: [[0, 22.4]], label: "Extreme Poverty" },
+    { data: [[1, 77.6]] }
+];
+
+window.onload = function () {
+    Flotr.draw(document.getElementById("chart"), data, {
+        title: "How much of the World lives on $1.25/Day?",
+        colors: ["#ff0000", "#6495ed"],
+        pie: {
+            show: true,
+            fillOpacity: 1
+        },
+        yaxis: {
+            showLabels: false
+        },
+        xaxis: {
+            showLabels: false
+        },
+        grid: {
+            horizontalLines: false,
+            verticalLines: false
+        }
+    });
+};
